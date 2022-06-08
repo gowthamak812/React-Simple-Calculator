fb21d9029a7f0a698020d80d0b6abe533cec48a0
Gowtham AK <gowthamak812@gmail.com>
2022-06-08 17:02:00 +0530

    mysecond commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className='Main m-0'>
       <Row className='justify-content-center ml-5 m-0'>
-        <Col xs={3} className='Main m-0'><h1>Calculator</h1></Col>  
-        <Col xs={3} className='m-0'><a target={'_blank'} href="https://github.com/gowthamak812/React-Simple-Calculator.git"><img style={{ width: 40 }} src={Github} /></a></Col>     
+        <Col xs={4} className='Main m-0'><h1>Calculator</h1></Col>  
+        <Col xs={4} className='m-0'><a target={'_blank'} href="https://github.com/gowthamak812/React-Simple-Calculator.git"><img style={{ width: 40 }} src={Github} /></a></Col>     
         </Row>
       <div className='color'>
       <Buttons />
