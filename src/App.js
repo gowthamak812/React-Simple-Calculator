3e706420d8a64fb9dfa682c7baf2be880684fc20
Gowtham AK <gowthamak812@gmail.com>
2022-06-08 16:56:10 +0530

    mysecond commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
- import Github from './images/Github.png';
+import Github from './images/Github.png';
+import 'bootstrap/dist/css/bootstrap.css';
+import {Row, Col } from 'react-bootstrap';
 
 import Buttons from './component/Buttons';
 function App() {
   return (
-    <div className='Main'>
-      <h1 style={{marginLeft: 70}}>Calculator<span><a target={'_blank'} href="https://github.com/gowthamak812/React-Simple-Calculator.git"><img style={{ width: 40 }} src={Github} /></a></span></h1>       
+    <div className='Main m-0'>
+      <Row className='justify-content-center ml-5 m-0'>
+        <Col xs={3} className='Main m-0'><h1>Calculator</h1></Col>  
+        <Col xs={3} className='m-0'><a target={'_blank'} href="https://github.com/gowthamak812/React-Simple-Calculator.git"><img style={{ width: 40 }} src={Github} /></a></Col>     
+        </Row>
       <div className='color'>
       <Buttons />
       </div>
