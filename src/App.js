762f1cdee0b0e15258d071e65ec2fd0693c7fd56
Gowtham AK <gowthamak812@gmail.com>
2022-06-08 16:07:34 +0530

    initial second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-// import './App.css';
+ import Github from './images/Github.png';
 
 import Buttons from './component/Buttons';
 function App() {
   return (
     <div className='Main'>
-      <h1>Calculator</h1>
+      <h1 style={{marginLeft: 70}}>Calculator<span><a target={'_blank'} href="https://github.com/gowthamak812/React-Simple-Calculator.git"><img style={{ width: 40 }} src={Github} /></a></span></h1>       
       <div className='color'>
-        <Buttons />
+      <Buttons />
       </div>
     </div>
   );
--- src/component/Buttons.js
+++ src/component/Buttons.js
@@ -17,7 +17,7 @@ export default function Buttons() {
       let tempResult = result.toString().slice(0, -1);
       setResult(tempResult);
     } else if (
-      ["/", "*", "+", "-", "%", "**"].includes(btnValue) &&
+      ["/", "*", "+", "-", "%"].includes(btnValue) &&
       result !== ""
     ) {
       setResult(result + btnValue);
@@ -27,6 +27,7 @@ export default function Buttons() {
     } else if (btnValue === "AC") {
       setResult("");
     }
+    
   }
 
   return (
@@ -35,33 +36,33 @@ export default function Buttons() {
         <input id='inp' type={Text} value={result} />
       </div>
       <div>
-        <Button variant="outline-success" className="rounded-circle crl" id="one" onClick={MyFunction} size="lg" active>AC</Button>{' '}
-        <Button variant="outline-success" className="rounded-circle crl " id="one" onClick={MyFunction} size="lg" active>%</Button>{' '}
-        <Button variant="outline-success" className="rounded-circle crl" id="one" onClick={MyFunction} size="lg" active>⌫</Button>{' '}
-        <Button variant="outline-secondary" className="rounded-circle crl" id="one" onClick={MyFunction} size="lg" active>/</Button>{' '}
+        <Button variant="outline-success" className="rounded-circle crl" onClick={MyFunction} size="lg" active>AC</Button>{' '}
+        <Button variant="outline-success" className="rounded-circle crl " onClick={MyFunction} size="lg" active>%</Button>{' '}
+        <Button variant="outline-success" className="rounded-circle crl" onClick={MyFunction} size="lg" active>⌫</Button>{' '}
+        <Button variant="outline-secondary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>/</Button>{' '}
       </div>
       <div>
-        <Button variant="outline-primary" className="rounded-circle crl" id="one" onClick={MyFunction} size="lg" active>7</Button>{' '}
-        <Button variant="outline-primary" className="rounded-circle crl" id="one" onClick={MyFunction} size="lg" active>8</Button>{' '}
-        <Button variant="outline-primary" className="rounded-circle crl" id="one" onClick={MyFunction} size="lg" active>9</Button>{' '}
-        <Button variant="outline-secondary" className="rounded-circle crl" id="one" onClick={MyFunction} size="lg" active>*</Button>{' '}
+        <Button variant="outline-primary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>7</Button>{' '}
+        <Button variant="outline-primary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>8</Button>{' '}
+        <Button variant="outline-primary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>9</Button>{' '}
+        <Button variant="outline-secondary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>*</Button>{' '}
       </div>
       <div>
-        <Button variant="outline-primary" className="rounded-circle crl" id="one" onClick={MyFunction} size="lg" active>4</Button>{' '}
-        <Button variant="outline-primary" className="rounded-circle crl" id="one" onClick={MyFunction} size="lg" active>5</Button>{' '}
-        <Button variant="outline-primary" className="rounded-circle crl" id="one" onClick={MyFunction} size="lg" active>6</Button>{' '}
-        <Button variant="outline-secondary" className="rounded-circle crl" id="one" onClick={MyFunction} size="lg" active>-</Button>{' '}
+        <Button variant="outline-primary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>4</Button>{' '}
+        <Button variant="outline-primary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>5</Button>{' '}
+        <Button variant="outline-primary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>6</Button>{' '}
+        <Button variant="outline-secondary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>-</Button>{' '}
       </div>
       <div>
-        <Button variant="outline-primary" className="rounded-circle crl" id="one" onClick={MyFunction} size="lg" active>1</Button>{' '}
-        <Button variant="outline-primary" className="rounded-circle crl" id="one" onClick={MyFunction} size="lg" active>2</Button>{' '}
-        <Button variant="outline-primary" className="rounded-circle crl" id="one" onClick={MyFunction} size="lg" active>3</Button>{' '}
-        <Button variant="outline-secondary" className="rounded-circle crl" id="one" onClick={MyFunction} size="lg" active>+</Button>{' '}
+        <Button variant="outline-primary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>1</Button>{' '}
+        <Button variant="outline-primary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>2</Button>{' '}
+        <Button variant="outline-primary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>3</Button>{' '}
+        <Button variant="outline-secondary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>+</Button>{' '}
       </div>
       <div>
-        <Button variant="outline-primary" className="btn-1 crl" id="one" onClick={MyFunction} size="lg" active>0</Button>{' '}
-        <Button variant="outline-primary" className="rounded-circle crl" id="one" onClick={MyFunction} size="lg" active>.</Button>{' '}
-        <Button variant="outline-danger" className="rounded-circle crl" id="one" onClick={MyFunction} size="lg" active>=</Button>{' '}
+        <Button variant="outline-primary" className="btn-1 crl" onClick={MyFunction} size="lg" active>0</Button>{' '}
+        <Button variant="outline-primary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>.</Button>{' '}
+        <Button variant="outline-danger" className="rounded-circle crl" onClick={MyFunction} size="lg" active>=</Button>{' '}
       </div>
     </>
   );
