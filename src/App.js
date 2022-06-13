cb6060666bf17d2bd9ab73341ee7a3845b1af9cb
Gowtham AK <gowthamak812@gmail.com>
2022-06-13 11:58:55 +0530

    adding functionalities

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import Github from './images/Github.png';
 import 'bootstrap/dist/css/bootstrap.css';
-import {Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 import Buttons from './component/Buttons';
 function App() {
   return (
-    <div className='Main m-0'>
+    <div className='main m-0'>
       <Row className='justify-content-center ml-5 m-0'>
-        <Col xs={4} className='Main m-0'><h1>Calculator</h1></Col>  
-        <Col xs={4} className='m-0'><a target={'_blank'} href="https://github.com/gowthamak812/React-Simple-Calculator.git"><img style={{ width: 40 }} src={Github} /></a></Col>     
-        </Row>
-      <div className='color'>
-      <Buttons />
+        <Col xs={4} className='main m-0'><h1>Calculator</h1></Col>
+        <Col xs={4} className='m-0'>
+          <a target={'_blank'} href="https://github.com/gowthamak812/React-Simple-Calculator.git">
+            <img src={Github} />
+          </a>
+        </Col>
+      </Row>
+      <div className='btn-div'>
+        <Buttons />
       </div>
     </div>
   );
--- src/component/Buttons.js
+++ src/component/Buttons.js
@@ -1,68 +1,123 @@
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../component/App.scss';
 
 
 export default function Buttons() {
-  let [result, setResult] = useState("");
-  function MyFunction(event) {
-    let btnValue = event.target.innerHTML;
-    if (
-      ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."].includes(btnValue)
-    ) {
-      setResult(result + btnValue);
-    } else if (btnValue === "⌫" && result !== "") {
-      let tempResult = result.toString().slice(0, -1);
-      setResult(tempResult);
-    } else if (
-      ["/", "*", "+", "-", "%"].includes(btnValue) &&
-      result !== ""
-    ) {
-      setResult(result + btnValue);
-    } else if (btnValue === "=") {
-      let tempres = eval(result);
-      setResult(tempres);
-    } else if (btnValue === "AC") {
-      setResult("");
+  const [preState, setPreState] = useState("");
+  const [curState, setCurState] = useState("");
+  const [input, setInput] = useState("0");
+  const [operator, setOperator] = useState(null);
+  const [total, setTotal] = useState(false);
+
+  const InputNum = (e) => {
+    if (curState.includes(".") && e.target.innerText === ".") return;
+    if (total) {
+      setPreState("");
     }
-    
-  }
+    curState
+      ? setCurState((pre) => pre + e.target.innerText)
+      : setCurState(e.target.innerText);
+    setTotal(false);
+  };
+  useEffect(() => {
+    setInput(curState);
+  }, [curState]);
+  useEffect(() => {
+    setInput("0");
+  }, []);
+  const OperatorType = (e) => {
+    setTotal(false);
+    setOperator(e.target.innerText);
+    if (curState === "") return;
+    if (preState !== "") {
+      Equals();
+    } else {
+      setPreState(curState);
+      setCurState("");
+    }
+  };
+  const Equals = (e) => {
+    if (e.target.innerText === "=") {
+      setTotal(true);
+    }
+    let cal;
+    switch (operator) {
+      case "/":
+        cal = String(parseFloat(preState) / parseFloat(curState));
+        break;
+      case "+":
+        cal = String(parseFloat(preState) + parseFloat(curState));
+        break;
+      case "*":
+        cal = String(parseFloat(preState) * parseFloat(curState));
+        break;
+      case "-":
+        cal = String(parseFloat(preState) - parseFloat(curState));
+        break;
+      default:
+        return;
+    }
+    setInput("");
+    setPreState(cal);
+    setCurState("");
+  };
+  const MinusPlus = () => {
+    if (curState.charAt(0) === "-") {
+      setCurState(curState.substring(1));
+    } else {
+      setCurState("-" + curState);
+    }
+  };
+
+  const Percent = () => {
+    preState
+      ? setCurState(String((parseFloat(curState) / 100) * preState))
+      : setCurState(String(parseFloat(curState) / 100));
+  };
+  const Reset = () => {
+    setPreState("");
+    setCurState("");
+    setInput("0");
+  };
+
 
   return (
     <>
       <div>
-        <input id='inp' type={Text} value={result} />
+        {input !== "" || input === "0" ? (
+          <input id="inp" value={input} />) : (<input id='inp' type={Text} value={preState} />)}
       </div>
       <div>
-        <Button variant="outline-success" className="rounded-circle crl" onClick={MyFunction} size="lg" active>AC</Button>{' '}
-        <Button variant="outline-success" className="rounded-circle crl " onClick={MyFunction} size="lg" active>%</Button>{' '}
-        <Button variant="outline-success" className="rounded-circle crl" onClick={MyFunction} size="lg" active>⌫</Button>{' '}
-        <Button variant="outline-secondary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>/</Button>{' '}
+        <Button variant="outline-success" className="rounded-circle" onClick={Reset} size="lg" active>AC</Button>{' '}
+        <Button variant="outline-success" className="rounded-circle " onClick={Percent} size="lg" active>%</Button>{' '}
+        <Button variant="outline-success" className="rounded-circle" onClick={MinusPlus} size="lg" active>+/-</Button>{' '}
+        <Button variant="outline-secondary" className="rounded-circle" onClick={OperatorType} size="lg" active>/</Button>{' '}
       </div>
       <div>
-        <Button variant="outline-primary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>7</Button>{' '}
-        <Button variant="outline-primary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>8</Button>{' '}
-        <Button variant="outline-primary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>9</Button>{' '}
-        <Button variant="outline-secondary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>*</Button>{' '}
+        <Button variant="outline-primary" className="rounded-circle" onClick={InputNum} size="lg" active>7</Button>{' '}
+        <Button variant="outline-primary" className="rounded-circle" onClick={InputNum} size="lg" active>8</Button>{' '}
+        <Button variant="outline-primary" className="rounded-circle" onClick={InputNum} size="lg" active>9</Button>{' '}
+        <Button variant="outline-secondary" className="rounded-circle" onClick={OperatorType} size="lg" active>*</Button>{' '}
       </div>
       <div>
-        <Button variant="outline-primary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>4</Button>{' '}
-        <Button variant="outline-primary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>5</Button>{' '}
-        <Button variant="outline-primary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>6</Button>{' '}
-        <Button variant="outline-secondary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>-</Button>{' '}
+        <Button variant="outline-primary" className="rounded-circle" onClick={InputNum} size="lg" active>4</Button>{' '}
+        <Button variant="outline-primary" className="rounded-circle" onClick={InputNum} size="lg" active>5</Button>{' '}
+        <Button variant="outline-primary" className="rounded-circle" onClick={InputNum} size="lg" active>6</Button>{' '}
+        <Button variant="outline-secondary" className="rounded-circle" onClick={OperatorType} size="lg" active>-</Button>{' '}
       </div>
       <div>
-        <Button variant="outline-primary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>1</Button>{' '}
-        <Button variant="outline-primary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>2</Button>{' '}
-        <Button variant="outline-primary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>3</Button>{' '}
-        <Button variant="outline-secondary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>+</Button>{' '}
+        <Button variant="outline-primary" className="rounded-circle" onClick={InputNum} size="lg" active>1</Button>{' '}
+        <Button variant="outline-primary" className="rounded-circle" onClick={InputNum} size="lg" active>2</Button>{' '}
+        <Button variant="outline-primary" className="rounded-circle" onClick={InputNum} size="lg" active>3</Button>{' '}
+        <Button variant="outline-secondary" className="rounded-circle" onClick={OperatorType} size="lg" active>+</Button>{' '}
       </div>
       <div>
-        <Button variant="outline-primary" className="btn-1 crl" onClick={MyFunction} size="lg" active>0</Button>{' '}
-        <Button variant="outline-primary" className="rounded-circle crl" onClick={MyFunction} size="lg" active>.</Button>{' '}
-        <Button variant="outline-danger" className="rounded-circle crl" onClick={MyFunction} size="lg" active>=</Button>{' '}
+        <Button variant="outline-primary" className="btn-1" onClick={InputNum} size="lg" active>0</Button>{' '}
+        <Button variant="outline-primary" className="rounded-circle btn-2" onClick={InputNum} size="lg" active>.</Button>{' '}
+        <Button variant="outline-danger" className="rounded-circle" onClick={Equals} size="lg" active>=</Button>{' '}
       </div>
     </>
   );
